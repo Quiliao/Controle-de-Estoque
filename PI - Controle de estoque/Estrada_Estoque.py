8511934a4ee4333c7f4ea67f2a90a21a4cf1a550
Quiliao <othavioquiliao@gmail.com>
2022-09-19 19:45:00 -0300

    Adicionado novas abas com bugs para ser tratados

--- PI - Controle de estoque/Estrada_Estoque.py
+++ PI - Controle de estoque/Estrada_Estoque.py
@@ -1,4 +1,4 @@
-from tkinter import font
+from tkinter import Button, font
 from tkinter.font import BOLD
 import PySimpleGUI as sg
 from numpy import flexible
@@ -8,7 +8,7 @@ from openpyxl import Workbook
 import pathlib
 
 
-sg.theme("DarkGrey2")
+sg.theme("Reddit")
 
 
 # Caminho do excel (pos import pandas as pd)
@@ -37,87 +37,121 @@ df = pd.read_excel(EXCEL_FILE)
 
 
 funcionarios = ["Carlos", "Lucia", "Leonardo", "Luana"]
-layout = [
-    [
-        sg.Text("Funcionario:", size=(15, 1), font=("Roboto", 12)),
-        sg.Push(),
-        sg.InputCombo(funcionarios, size=(20, 1), key="-FUNCIONARIO-"),
-        sg.Push(),
-        sg.CalendarButton(
-            "Data",
-            target="-ADICIONADO-",
-            format="%d/%m/%y",
-            font=("Roboto", 10, BOLD),
-        ),
-        sg.InputText(size=(10, 1), key="-ADICIONADO-"),
-        sg.Push(),
-        sg.CalendarButton(
-            "Vencimento",
-            target="-VENCE-",
-            format="%d/%m/%y",
-            font=("Roboto", 10, BOLD),
-        ),
-        sg.InputText(size=(8, 1), key="-VENCE-"),
-    ],
-    [
-        sg.Text("Codigo de barra:", size=(13, 1), font=("Roboto", 12)),
-        sg.InputText(size=(26, 1), key="-CODIGO-", font=("Roboto", 12)),
-        sg.Push(),
-        sg.Text("Quantidade:", size=(9, 1), font=("Roboto", 12)),
-        sg.InputText(size=(11, 1), key="-QUANTIDADE-", font=("Roboto", 12)),
-    ],
-    [
-        sg.Text("Nome do produto:", size=(14, 1), font=("Roboto", 12)),
-        sg.InputText(size=(25, 1), key="-PRODUTO-", font=("Roboto", 12)),
-        sg.Push(),
-        sg.Text("Preco: ", size=(5, 1), font=("Roboto", 12)),
-        sg.InputText(size=(11, 1), key="-PRECO-", font=("Roboto", 12)),
-    ],
-    [sg.VPush()],
-    [
-        sg.Submit(
-            "Adicionar",
-            size=(15, 1),
-            font=("Roboto", 12, BOLD),
-            button_color=("black", "green"),
-        ),
-        sg.Button("Limpar", size=(15, 1), font=("Roboto", 12, BOLD)),
-        sg.Exit(
-            "Sair",
-            size=(15, 1),
-            font=("Roboto", 12, BOLD),
-            button_color=("black", "red"),
-        ),
-    ],
-]
 
 
-window = sg.Window(
-    "Entrada de estoque",
-    layout,
-    size=(650, 150),
-    element_justification="center",
-)
+def janela_login():
+    sg.theme("Reddit")
+    layout = [
+        [
+            [sg.Text("Usuario"), sg.Input(key="usuario", size=(20, 1))],
+            [sg.Button("Continuar"), sg.Button("Sair")],
+        ]
+    ]
+
+    return sg.Window("Login", layout=layout, finalize=True)
 
 
-def clear_input():
-    for key in values:
-        window[key].update(" ")
-    return None
+def janela_principal():
+    sg.theme("Reddit")
+    layout = [
+        [sg.Text("Bem vindo ao sistema de estoque", font=("Arial", 20))],
+        [sg.Text("Escolha uma opção abaixo:", font=("Arial", 15))],
+        [sg.Button("Adicionar produto", size=(20, 1))],
+        [sg.Button("Visualizar estoque", size=(20, 1))],
+        [sg.Button("Verificar vencimentos", size=(20, 1))],
+        [sg.Button("Voltar", size=(10, 1)), sg.Button("Sair", size=(10, 1))],
+    ]
+    return sg.Window("Estoque", layout=layout, finalize=True)
 
+
+def Adicionar_Produtos():
+    sg.theme("Reddit")
+    layout = [
+        [
+            sg.Text("Funcionario:", size=(15, 1), font=("Roboto", 12)),
+            sg.Push(),
+            sg.InputCombo(funcionarios, size=(20, 1), key="-FUNCIONARIO-"),
+            sg.Push(),
+            sg.CalendarButton(
+                "Data",
+                target="-ADICIONADO-",
+                format="%d/%m/%y",
+                font=("Roboto", 10, BOLD),
+            ),
+            sg.InputText(size=(10, 1), key="-ADICIONADO-"),
+            sg.Push(),
+            sg.CalendarButton(
+                "Vencimento",
+                target="-VENCE-",
+                format="%d/%m/%y",
+                font=("Roboto", 10, BOLD),
+            ),
+            sg.InputText(size=(8, 1), key="-VENCE-"),
+        ],
+        [
+            sg.Text("Codigo de barra:", size=(13, 1), font=("Roboto", 12)),
+            sg.InputText(size=(26, 1), key="-CODIGO-", font=("Roboto", 12)),
+            sg.Push(),
+            sg.Text("Quantidade:", size=(9, 1), font=("Roboto", 12)),
+            sg.InputText(size=(11, 1), key="-QUANTIDADE-", font=("Roboto", 12)),
+        ],
+        [
+            sg.Text("Nome do produto:", size=(14, 1), font=("Roboto", 12)),
+            sg.InputText(size=(25, 1), key="-PRODUTO-", font=("Roboto", 12)),
+            sg.Push(),
+            sg.Text("Preco: ", size=(5, 1), font=("Roboto", 12)),
+            sg.InputText(size=(11, 1), key="-PRECO-", font=("Roboto", 12)),
+        ],
+        [sg.VPush()],
+        [
+            sg.Submit(
+                "Adicionar",
+                size=(15, 1),
+                font=("Roboto", 12, BOLD),
+                button_color=("white", "green"),
+            ),
+            sg.Button("Limpar", size=(15, 1), font=("Roboto", 12, BOLD)),
+            sg.Exit(
+                "Sair",
+                size=(15, 1),
+                font=("Roboto", 12, BOLD),
+                button_color=("white", "red"),
+            ),
+        ],
+    ]
+    return sg.Window("Adicionar produtos", layout=layout, finalize=True)
+
+
+janela1, janela2, janela3 = janela_login(), None, None
 
 while True:
-    event, values = window.read()
-    if event == sg.WIN_CLOSED or event == "Sair":
+    window, event, values = sg.read_all_windows()
+    if window == janela1 and event == sg.WIN_CLOSED:
+        break
+    if window == janela1 and event == "Continuar":
+        if values["usuario"] == "admin":
+            janela1.hide()
+            janela2 = janela_principal()
+        else:
+            sg.popup("Usuario invalido")
+    if window == janela2 and event == sg.WIN_CLOSED:
         break
 
-    if event == "Limpar":
-        clear_input()
-
-    if event == "Adicionar":
-        df = df.append(values, ignore_index=True)
-        df.to_excel(EXCEL_FILE, index=False)
-        sg.popup("Item adicionado com sucesso!")
-        clear_input()
+    if (
+        window == janela2 and event == "Adicionar produto"
+    ):  # Vai para a aba Adicionar produto
+        janela2.hide()
+        janela3 = Adicionar_Produtos()
+        if (
+            event == sg.WIN_CLOSED or event == "Sair"
+        ):  # Sair da janela de adicionar produtos
+            break
+        if event == "Limpar":  # Limpar campos da janela de adicionar produtos
+            clear_input()
+        if event == "Adicionar":  # Adicionar produto no excel
+            df = df.append(values, ignore_index=True)
+            df.to_excel(EXCEL_FILE, index=False)
+            sg.popup("Item adicionado com sucesso!")
+            clear_input()
 
 window.close()
